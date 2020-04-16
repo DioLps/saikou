90f62da0b1e1f3d5371e7696f4c2f821b05e0d56
diolps <diolopesrln@gmail.com>
2020-04-16 00:14:55 -0300

    fix pipe

--- apps/saikou-animes/src/app/shared/pipes/add-base-url-to-cover.pipe.ts
+++ apps/saikou-animes/src/app/shared/pipes/add-base-url-to-cover.pipe.ts
@@ -5,6 +5,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class AddBaseUrlToCoverPipe implements PipeTransform {
   transform(cover: string): string {
+    cover = cover.replace('https://goyabu.com/', '');
     return !cover.includes('https://goyabu.com/')
       ? 'https://goyabu.com/' + cover
       : cover;
