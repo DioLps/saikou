673e239177c4e28247ecc538a89dc9f1c7b786cf
diolps <diolopesrln@gmail.com>
2020-06-04 11:24:42 -0300

    test

--- update-master-by-dev.bat
+++ update-master-by-dev.bat
@@ -1,1 +1,9 @@
-git pull | git push | git checkout release | git merge dev | git push | git checkout master | git merge release | git push | git checkout dev
+git pull
+git push
+git checkout release
+git merge dev
+git push
+git checkout master
+git merge release
+git push
+git checkout dev
